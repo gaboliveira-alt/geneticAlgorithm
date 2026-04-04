f7f9019d6411fb3f314c842387f6ef199dd11b04
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-04-03 21:49:20 -0300

    move 6 files and update 1 file

--- bf2/mutation.py
+++ bf2/mutation.py
@@ -1,7 +1,7 @@
 import random
 
 def apply_mutation(individual: list[float], lower_bound: float, upper_bound: float) -> list[float]:
-    if random.random() < 0.4:
+    if random.random() < 0.3:
         gene_to_mutate: int = random.randint(0, 1)
         individual[gene_to_mutate] = random.uniform(lower_bound, upper_bound)
         individual[2] = 0.0

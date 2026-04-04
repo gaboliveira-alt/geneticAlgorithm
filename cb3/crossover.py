9708a595850cfb969b0e0e3025f82aaf2cd8d5ee
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2026-04-03 22:10:00 -0300

    update crossover.py and mutation.py

--- cb3/crossover.py
+++ cb3/crossover.py
@@ -1,5 +1,5 @@
 def crossover(parent_01: list[float], parent_02: list[float]) -> list[float]:
-    cross_rate = 0.8
+    cross_rate = 0.6
     
     child_x1 = (parent_01[0] * cross_rate) + (parent_02[0] * (1 - cross_rate))
     child_x2 = (parent_02[1] * cross_rate) + (parent_02[1] * (1 - cross_rate))
